32ebdbbb9e33cd870d456d149ccf4e3cae372879
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-12-21 15:34:05 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  theme: {
+    extend: {
+      boxShadow: {
+        outline: '0 0 0 1px rgba(66, 153, 225, 0.5)',
+      },
+      colors: {
+        'orange-button': '#FF872E',
+        'green-button': '#1ABC9C',
+        'purple-hover': '#4C52F8',
+        'purple-hover-stroke': '#8286FF',
+        'purple-progress': '#3B41E3',
+      },
+      borderRadius: {
+        20: '20px',
+      },
+    },
+  },
+  fontFamily: {
+    sans: ['Poppins', 'sans-serif'],
+  },
+  variants: {},
+  plugins: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+    ],
+  },
+}
